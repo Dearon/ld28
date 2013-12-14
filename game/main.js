6e3c268d1541f7a6bb96446edcf64ba49b2cd326
The-Skas <skas@live.co.uk>
2013-12-14 16:22:11 +0000

    added map drawing and level drawing.

--- game/main.js
+++ game/main.js
@@ -15,7 +15,7 @@ function init() {
 	stage.update();
 
 	level = levelGeneration(); 
-	ilevel = level.length; jlevel = level.length;
+	ilevel = Math.floor(level.length/2); jlevel = Math.floor(level.length/2)	;
 	$.each(level, function(index, item) {
 		$.each(item, function(innerIndex, innerItem) {
 			if (innerItem) {
@@ -36,25 +36,63 @@ function update(event)
 {
 	//TODO:Player access index.
 	stage.removeAllChildren();
+	drawBackground();
 	drawMap();
 	stage.update();
 }
 
-function drawMap()
+function drawBackground()
 {
 
-	// stage.addChild(bg);
-	// var bg = new createjs.Shape();
-	// bg.graphics.beginFill('#BBB').drawRect(STAGE_WIDTH/4, STAGE_HEIGHT/4, 400, 300);
-	// stage.addChild(bg);
+	stage.addChild(bg);
+	var bg = new createjs.Shape();
+	bg.graphics.beginFill('#BBB').drawRect(STAGE_WIDTH/4, STAGE_HEIGHT/4, 400, 300);
+	stage.addChild(bg);
 
-	// //Draw door ways.
-	// var tempi = ilevel, tempj = jlevel;
-	// if(tempi > 0)
-	// {
-	// 	console.log("THERE IS A PATH!");
-	// }
-
-
+	//Draw door ways.
+	if(ilevel <= level.length && ilevel >= 0 && jlevel <=level.length && jlevel >= 0)
+	{	
+		//left
+		if(checkBounds(ilevel - 1, jlevel) && level[ilevel-1][jlevel])
+		{
+			bg.graphics.beginFill('#AAA').drawRect(0, STAGE_HEIGHT/2-50, 200, 100);
+		}
+		//top
+		if(checkBounds(ilevel, jlevel - 1) && level[ilevel][jlevel-1])
+		{
+			bg.graphics.beginFill('#AAA').drawRect(STAGE_WIDTH/2-50, 0, 100, 150);
+		}
+		//right
+		if(checkBounds(ilevel+1, jlevel) && level[ilevel+1][jlevel])
+		{
+			bg.graphics.beginFill('#AAA').drawRect(STAGE_WIDTH, STAGE_HEIGHT/2-50, -200, 100);
+		}
+		//below
+		if(checkBounds(ilevel, jlevel+1) && level[ilevel][jlevel+1])
+		{
+			bg.graphics.beginFill('#AAA').drawRect(STAGE_WIDTH/2-50, STAGE_HEIGHT, 100, -150);
+		}
+	}
 }
+function checkBounds(i, j)
+{
+	return(i <= level.length && i >= 0 && j <=level.length && j >= 0)
+}
+
+function drawMap()
+{
+	$.each(level, function(index, item) {
+		$.each(item, function(innerIndex, innerItem) {
+			if (innerItem) {
+				var top = 20 + (20 * index);
+				var left = 20 + (20 * innerIndex);
+
+				var block = new createjs.Shape();
+				block.graphics.beginFill('#fff').drawRect(left, top, 10, 10);
+				stage.addChild(block);
+			}
+		});
+	});
+}
+
 
