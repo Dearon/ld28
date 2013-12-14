300ded1799c4879310abee7d451f38682de7381a
Dearon <dearon@gmail.com>
2013-12-14 16:07:45 +0100

    Fixed a bug where the array tried to access a non-existent index

--- game/levelGeneration.js
+++ game/levelGeneration.js
@@ -17,56 +17,36 @@ function levelGeneration() {
 
 	level[y][x] = true;
 
-	level = levelSpider(level, x, y, 80);
-
-	/*levelSpider(100);
-
-	for (var i = 0; i < 4; i++) {
-		var chance = 100;
-
-		if (i == 1) {
-			while(chance != 0) {
-				var random = Math.round(Math.random() * (100 - 0) + 0);
-				if (random <= chance) {
-					console.log('Yay');
-				}
-
-				chance-= 5;
-			}
-		}
-	}*/
+	level = levelSpider(level, x, y, 100, 5);
 
 	return level;
 }
 
-function levelSpider(level, x, y, chance) {
+function levelSpider(level, x, y, chance, chanceDecrease) {
 	level[y][x] = true;
 
-	if (! level[y - 1][x]) {
-		var random = Math.round(Math.random() * (100 - 0) + 0);
-		if (random <= chance) {
-			level = levelSpider(level, x, y - 1, chance - 5);
+	var width = level.length;
+	var height = level[0].length;
+
+	for (var i = -1; i < 2; i = i + 2) {
+		if ((y + i) != 0 && (y + i) != height) {
+			if (level[y + i][x] == false) {
+				var random = Math.round(Math.random() * (100 - 0) + 0);
+				if (random <= chance) {
+					level = levelSpider(level, x, y + i, chance - chanceDecrease, chanceDecrease);
+				}
+			}
 		}
 	}
 
-	if (! level[y][x + 1]) {
-		var random = Math.round(Math.random() * (100 - 0) + 0);
-		if (random <= chance) {
-			level = levelSpider(level, x + 1, y, chance - 5);
-		}
-	}
-
-	if (! level[y + 1][x]) {
-		var random = Math.round(Math.random() * (100 - 0) + 0);
-		if (random <= chance) {
-			level = levelSpider(level, x, y + 1, chance - 5);
-		}
-	}
-
-	if (! level[y][x -1]) {
-		var random = Math.round(Math.random() * (100 - 0) + 0);
-		if (random <= chance) {
-			level = levelSpider(level, x - 1, y, chance - 5);
+	for (var i = -1; i < 2; i = i + 2) {
+		if ((x + i) != 0 && (x + i) != height) {
+			if (level[y][x + i] == false) {
+				var random = Math.round(Math.random() * (100 - 0) + 0);
+				if (random <= chance) {
+					level = levelSpider(level, x + i, y, chance - chanceDecrease, chanceDecrease);
+				}
+			}
 		}
 	}
 
