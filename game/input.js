792208d06b3e936f4e844c75a6ba75d72a711b51
Dearon <dearon@gmail.com>
2013-12-14 19:40:05 +0100

    Removed unneeded calls to drawPlayerMap

--- game/input.js
+++ game/input.js
@@ -4,18 +4,14 @@ function playerMovement(stage, level) {
 
 	Mousetrap.bind('up', function() {
 		player.x-=1;
-		drawPlayerMap(stage, level, x, y);
 	});
 	Mousetrap.bind('down', function() {
 		player.x+=1;
-		drawPlayerMap(stage, level, x, y);
 	});
 	Mousetrap.bind('left', function() {
 		player.y-=1;
-		drawPlayerMap(stage, level, x, y);
 	});
 	Mousetrap.bind('right', function() {
 		player.y+=1;
-		drawPlayerMap(stage, level, x, y);
 	});
 }
