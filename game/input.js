0b22ff5f1e3db4b9b1fe35981338619d0a3fccc5
Dearon <dearon@gmail.com>
2013-12-16 19:34:44 +0100

    Prevent default behavior from firing

--- game/input.js
+++ game/input.js
@@ -4,20 +4,26 @@ function input() {
 
 	Mousetrap.bind('up', function() {
 		player.move('up');
+		return false;
 	});
 	Mousetrap.bind('down', function() {
 		player.move('down');
+		return false;
 	});
 	Mousetrap.bind('left', function() {
 		player.move('left');
+		return false;
 	});
 	Mousetrap.bind('right', function() {
 		player.move('right');
+		return false;
 	});
 	Mousetrap.bind('ctrl', function() {
 		player.selectNextEnemy();
+		return false;
 	});
 	Mousetrap.bind('space', function() {
 		player.attackEnemy();
+		return false;
 	});
 }
