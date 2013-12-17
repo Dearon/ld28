18fadd58c29e9208d87caeaf8f9aa80417551653
Dearon <dearon@gmail.com>
2013-12-17 02:35:28 +0100

    Less chance for chests to spawn

--- game/itemTools.js
+++ game/itemTools.js
@@ -3,7 +3,7 @@ var itemTools = {
 		for(var i = 0; i < level.length; i++) {
 			for(var j = 0; j < level[0].length; j++) {
 				if(level[i][j] && ! level[i][j].exit && level[i][j].enemies.length == 0) {
-					if (Math.random() >= 0.50) {
+					if (Math.random() >= 0.75) {
 						var chest = items.chest();
 						level[i][j].items.push(chest);
 					}
